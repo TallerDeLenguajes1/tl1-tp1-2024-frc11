c1454f28cbcd1f850363e35f06195e5a221d8914
Franco <francopizzi2003@gmail.com>
2024-04-08 09:32:27 -0300

    segundo commit

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
  - A
  Conviene incluirlo puesto que nos sirve para tener organizado que archivos queremos que se ignoren al usar git, se debe utilizar siempre a la hora de iniciar un proyecto. Para crear un archivo .gitignore local, se crea un archivo de texto y se le asígna el nombre .gitignore y se lo edita segun sea necesario.
 - Pregunta 3
+ - C
+ Se ve el archivo README.md, tp1_1.cpp y tp1_1.exe, yo creo que no haria falta que este el ejecutable .exe
--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,4 +1,14 @@
 #include <stdio.h>
 main(){
     printf("hello world");
+    int num = 11; 
+    int *puntero = &num; /
+
+    printf("El contenido del puntero: %d\n", *puntero); // Imprimimos el contenido del puntero
+    printf("La dirección de memoria almacenada por el puntero: %p\n", puntero); // Imprimimos la dirección de memoria almacenada por el puntero
+    printf("La dirección de memoria de la variable: %p\n", &num); // Imprimimos la dirección de memoria de la variable num
+    printf("La dirección de memoria del puntero: %p\n", &puntero); // Imprimimos la dirección de memoria del puntero
+    printf("El tamaño de memoria utilizado por esa variable usando la función sizeof(): %lu\n", sizeof(num)); // Imprimimos el tamaño de memoria utilizado por la variable num usando la función sizeof()
+
+    return 0;
 }
